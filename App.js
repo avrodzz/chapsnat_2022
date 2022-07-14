1235481c02ba6f4d7b196986ea2b306cf48278a1
Alexis Rodriguez <avrodzz@gmail.com>
2022-07-14 14:36:01 -0700

    Before adding navigations - the app should update the database with the messages entered

--- App.js
+++ App.js
@@ -1,51 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState, useCallback, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import 'react-native-gesture-handler';
 import { GiftedChat } from 'react-native-gifted-chat'
+import db from "./firebase";
+import { collection, getDocs, setDoc, updateDoc, doc, arrayUnion, onSnapshot } from 'firebase/firestore';
 
 export default function App() {
   const [messages, setMessages] = useState([]);
 
-  useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: 'Hello Alexis!',
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'Bot #1',
-          avatar: 'https://placeimg.com/140/140/any',
-        },
-      },
-      {
-        _id: 3,
-        text: 'How are you doing today?',
-        createdAt: new Date(2022, 7, 12, 11, 45, 0),
-        user: {
-          _id: 4,
-          name: 'Bot #1',
-          avatar: 'https://placeimg.com/140/140/any',
-        },
-      },
-    ])
-  }, [])
+  // useEffect(() => {
+  //   async function getChat() {
+  //     console.log("starting get!")
+  //     const chatsCol = collection(db, 'Chats');
+  //     const chatsDoc = await getDocs(chatsCol);
+  //     const chatData = chatsDoc.docs.map(doc => doc.data());
+  //     console.log("here chatData", chatData);
+  //     setMessages(messages);
+  //   }
 
-  const onSend = useCallback((messages = []) => {
+  //   getChat();
+  // }, []);
+
+  // useEffect(() => {
+  //   onSnapshot(doc(db, "Chats", "myfirstchat"), (snapshot) => {
+  //     console.log("New Snapshot! ", snapshot.data().messages);
+  //     setMessages(snapshot.data().messages);
+  //   });
+  // }, []);
+
+  useEffect(() => {
+    let unsubscribeFromNewSnapshots = onSnapshot(doc(db, "Chats", "myfirstchat"), (snapshot) => {
+      console.log("New Snapshot! ", snapshot.data().messages);
+      setMessages(snapshot.data().messages);
+    });
+  
+    return function cleanupBeforeUnmounting() {
+      unsubscribeFromNewSnapshots();
+    };
+  }, []);
+
+  // const onSend = useCallback((messages = []) => {
+  //   setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+  // }, [])
+
+  const onSend = useCallback(async (messages = []) => {
+    await updateDoc(doc(db, "Chats", "myfirstchat"), {
+      messages: arrayUnion(messages[0])
+    });
     setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
-  }, [])
+}, [])
 
   return (
     <GiftedChat
       messages={messages}
       onSend={messages => onSend(messages)}
+      // user={{
+      //   _id: 1,
+      //   name: "Alexis Rodriguez",
+      //   // Make sure the link is not broken!
+      //   avatar: "https://dogtime.com/assets/uploads/2016/09/rottweiler-puppy-1-e1553630142158-1280x720.jpg",
+      // }}
       user={{
-        _id: 1,
-        name: "Alexis Rodriguez",
-        // Make sure the link is not broken!
-        avatar: "https://dogtime.com/assets/uploads/2016/09/rottweiler-puppy-1-e1553630142158-1280x720.jpg",
+        // current "blue bubble" user
+        _id: "1",
+        name: "Ashwin",
+        avatar: "https://placeimg.com/140/140/any",
       }}
-      placeholder={"Type Thee Message!"}
+      inverted={true}
+      // placeholder={"Type Thee Message!"}
       showUserAvatar={true}
       alwaysShowSend={true}
       renderUsernameOnMessage={true}
