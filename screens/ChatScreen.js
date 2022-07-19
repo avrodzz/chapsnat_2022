62369b31a0bc4a263550dd63749db72456237329
Alexis Rodriguez <avrodzz@gmail.com>
2022-07-19 12:02:17 -0700

    Updated my project according to Stack Navigator Lab

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,35 +1,27 @@
 import React, { useState, useCallback, useEffect } from "react";
 import { GiftedChat } from "react-native-gifted-chat";
-import db from "../firebase";
+import db from "./firebase";
 import firebase from "firebase/app";
 
 export default function ChatScreen({ navigation }) {
   const [messages, setMessages] = useState([]);
 
-  useEffect(() => {
-    let unsubscribeFromNewSnapshots = db
-      .collection("Chats")
-      .doc("myfirstchat")
-      .onSnapshot((snapshot) => {
-        console.log("New Snapshot!");
-        setMessages(snapshot.data().messages);
-      });
-
+    useEffect(() => {
+    let unsubscribeFromNewSnapshots = onSnapshot(doc(db, "Chats", "myfirstchat"), (snapshot) => {
+      console.log("New Snapshot! ", snapshot.data().messages);
+      setMessages(snapshot.data().messages);
+    });
+  
     return function cleanupBeforeUnmounting() {
       unsubscribeFromNewSnapshots();
     };
   }, []);
 
-  const onSend = useCallback((messages = []) => {
-    db.collection("Chats")
-      .doc("myfirstchat")
-      .update({
-        // arrayUnion appends the message to the existing array
-        messages: firebase.firestore.FieldValue.arrayUnion(messages[0]),
-      });
-    setMessages((previousMessages) =>
-      GiftedChat.append(previousMessages, messages)
-    );
+  const onSend = useCallback(async (messages = []) => {
+    await updateDoc(doc(db, "Chats", "myfirstchat"), {
+      messages: arrayUnion(messages[0])
+    });
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
   }, []);
 
   return (
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,46 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { FlatList, Text, View, TouchableOpacity, StyleSheet } from "react-native";
-import db from "../firebase";
+import db from "./firebase";
 
 export default function HomeScreen({ navigation }) {
-  const [chatList, setChatList] = useState([]);
-
-//   useEffect(() => {
-//     let chatsRef = db.collection("Chats");
-//     chatsRef.get().then((querySnapshot) => {
-//       let newChatList = [];
-//       querySnapshot.forEach((doc) => {
-//         let newChat = { ...doc.data() };
-//         newChat.id = doc.id;
-//         newChatList.push(newChat);
-//         console.log(newChatList);
-//       });
-//       setChatList(newChatList);
-//     });
-//   }, []);
-
-useEffect(() => {
-    async function getChat() {
-        console.log("starting get!")
-        const chatsCol = collection(db, 'Chats');
-        const chatsDoc = await getDocs(chatsCol);
-        const chatData = chatsDoc.docs.map(doc => doc.data());
-        console.log("here chatData", chatData);
-        setMessages(messages);
-    }
 
   return (
     <View style={styles.container}>
-      <FlatList
-        data={chatList}
-        renderItem={({ item }) => (
-          <TouchableOpacity
-            onPress={() => navigation.navigate("Chat")}
-          >
-            <Text style={styles.item}>{item.id}</Text>
+	  <TouchableOpacity
+	    onPress={() => navigation.navigate("Chat")}
+	  >
+          	<Text style={styles.item}>{item.id}</Text>
           </TouchableOpacity>
-        )}
-      />
     </View>
   );
 }
